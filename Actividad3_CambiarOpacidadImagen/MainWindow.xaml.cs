19684e1f66c2697509f1b49fa82a378419f02b13
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-11-18 12:31:52 +0100

    Agregar archivos de proyecto.

---
+++ Actividad3_CambiarOpacidadImagen/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Actividad3_CambiarOpacidadImagen
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void altaRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Opacity = 0.9;
+        }
+
+        private void mediaRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Opacity = 0.6;
+        }
+
+        private void bajaRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Opacity = 0.4;
+        }
+
+        private void uniformeRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Stretch = Stretch.Uniform;
+        }
+
+        private void rellenoUniformeRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Stretch = Stretch.UniformToFill;
+        }
+
+        private void sinAjusteRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Stretch = Stretch.None;
+        }
+
+        private void rellenoRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            imagenImage.Stretch = Stretch.Fill;
+        }
+    }
+}
